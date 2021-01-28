c3269d1e2d963dc731ccc8efac10a8ae4e788a0c
Obed Otto <obedotto@saveetha.ac.in>
2021-01-28 01:26:29 +0000

    calculations are completed

--- scriptproject/urls.py
+++ scriptproject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mathapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('mathadd/',views.mathadd,name='mathadd'),
 ]
